9a509ba23bcf5220a243294fc8fc7454bb5712f3
Shafique Mohammad <shafique@Shafiques-MacBook-Pro.local>
2024-11-09 10:56:20 +0530

    Initial commit

---
+++ src/main/java/org/shafique/driver/Main.java
@@ -0,0 +1,9 @@
+package org.shafique.driver;
+
+import org.shafique.parkinglot.ParkingLotDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        ParkingLotDriver.driver();
+    }
+}
---
+++ src/main/java/org/shafique/parkinglot/ParkingLotDriver.java
@@ -0,0 +1,7 @@
+package org.shafique.parkinglot;
+
+public class ParkingLotDriver {
+    public static void driver() {
+        System.out.println("Implement driver for parking lot.");
+    }
+}
